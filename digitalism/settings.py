082a2ec9642db1b0ea1db47941d6a1cc2c1413b9
Su-Korawit <o.korawitt@gmail.com>
2025-01-01 19:53:46 +0700

    security api key

--- digitalism/settings.py
+++ digitalism/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'digitalism',
         'USER': 'root',
-        'PASSWORD': 'root64495',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
